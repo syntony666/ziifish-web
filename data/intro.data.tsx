de2cecc3543adb9c6a9b67f7aedeef4d4425c3d5
Tony <admin@syntony666.com>
2022-12-27 13:09:14 +0800

    change data binding way

---
+++ data/intro.data.tsx
@@ -0,0 +1,48 @@
+import { IntroModel } from '../model/intro.model';
+
+export const introData: IntroModel[] = [
+  {
+    name: '仔魚',
+    description: (
+      <>
+        HI~我是個人勢Vtuber,沒有任何公司經紀約
+        <br />
+        英文叫ziifish,中文:仔(ㄗˇ )魚 ,
+      </>
+    ),
+    avatarImg: '/intro/ziifish-avatar.webp',
+    logoImg: '/intro/ziifish-logo.webp',
+    shopUrl: '/shop/ziifish',
+    twitterUrl: 'https://twitter.com/linziifish',
+    twitchUrl: 'https://www.twitch.tv/ziifish',
+    youtubeUrl: 'https://www.youtube.com/@ziifish',
+    instagramUrl: 'https://www.instagram.com/zii_fish/',
+    facebookUrl: 'https://www.facebook.com/linziifish',
+  },
+  {
+    name: '若娜',
+    description: (
+      <>
+        大家好 我是若娜，也可以叫我娜娜!!
+        <br />
+        主要還是以TWITCH實況直播為主
+      </>
+    ),
+    avatarImg: '/intro/zona-avatar.webp',
+    logoImg: '/intro/zona-logo.webp',
+    shopUrl: '/shop/zona',
+    twitterUrl: 'https://twitter.com/ZonaHart13',
+    twitchUrl: 'https://www.twitch.tv/zona181227',
+    youtubeUrl: 'https://www.youtube.com/@zona7212',
+    instagramUrl: 'https://www.instagram.com/zona181227/',
+  },
+  {
+    name: '海鹽',
+    description: <>團戰0貢獻 輸出全靠鞭 CS很強的台 但不會開CS 可以叫我海鹽</>,
+    avatarImg: '/intro/doodoo-avatar.webp',
+    logoImg: '/intro/doodoo-logo.webp',
+    shopUrl: '/shop/doodoo',
+    twitchUrl: 'https://www.twitch.tv/doodooqvvq',
+    instagramUrl: 'https://www.instagram.com/doodookiiiiii87/',
+  },
+];
---
+++ model/intro.model.ts
@@ -0,0 +1,12 @@
+export interface IntroModel {
+    name: string,
+    description: JSX.Element,
+    avatarImg: string,
+    logoImg: string,
+    shopUrl: string,
+    twitterUrl?: string,
+    twitchUrl?: string,
+    youtubeUrl?: string,
+    instagramUrl?: string,
+    facebookUrl?: string
+};
